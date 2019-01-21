17b22824a4cc67780c42121230e0db7be5175d8a
Miguel Urbina <miguel.urbina@michelada.io>
2019-01-21 15:38:30 -0600

    Now user can wich type of activity is uploading

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,4 +1,5 @@
 class Activity < ApplicationRecord
   belongs_to :user
   scope :user_activities, ->(actual_user) { where(user_id: actual_user) }
+  enum type: { post: 0, platica: 1, curso: 3 }
 end
---
+++ db/migrate/20190121213152_add_type_to_activity.rb
@@ -0,0 +1,5 @@
+class AddTypeToActivity < ActiveRecord::Migration[5.2]
+  def change
+    add_column :activities, :type, :integer
+  end
+end
