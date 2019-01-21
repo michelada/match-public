cc1911a72897c4b23fd2096f123d7d986d61ba6f
Miguel Urbina <miguel.urbina@michelada.io>
2019-01-21 14:24:44 -0600

    Extra controller deleted

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -1,10 +1,11 @@
 # Adding function to invitation controller.
-#
-class Users::InvitationsController < Devise::InvitationsController
-  after_action :update_user_account, only: [:create]
+module Users
+  class InvitationsController < Devise::InvitationsController
+    after_action :update_user_account, only: [:create]
 
-  def update_user_account
-    @user = User.find_by_email(params[:user][:email])
-    @user.update_attribute(:team_id, current_user.team_id)
+    def update_user_account
+      @user = User.find_by_email(params[:user][:email])
+      @user.update_attribute(:team_id, current_user.team_id)
+    end
   end
 end
