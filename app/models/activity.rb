d29c55e3dc29bd34432fae199329f4cb95f3dfba
Miguel Urbina <miguel.urbina@michelada.io>
2019-01-21 17:28:08 -0600

    User model validation added

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -3,4 +3,5 @@ class Activity < ApplicationRecord
   scope :user_activities, ->(actual_user) { where(user_id: actual_user) }
   enum activity_type: { Curso: 0, Platica: 1, Post: 2 }
   validates :name, presence: true, uniqueness: { case_sensitive: false }
+  validates :name, :english, :location, precense: true
 end
