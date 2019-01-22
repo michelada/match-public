6410f4cd234e3c1a23c832533baf45e26c6a0afc
Leyaim Jimenez <leyaim.jimenez@michelada.io>
2019-01-22 16:23:29 -0600

    Adding apis controller

---
+++ app/controllers/api/apis_controller.rb
@@ -0,0 +1,4 @@
+module Api
+  class ApisController < ApplicationController
+  end
+end
--- db/migrate/20190120053114_create_teams.rb
+++ db/migrate/20190120053114_create_teams.rb
@@ -3,6 +3,7 @@ class CreateTeams < ActiveRecord::Migration[5.2]
   def change
     create_table :teams do |t|
       t.string :name, null: false
+      t.integer :score
 
       t.timestamps
     end
