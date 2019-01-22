b098dde6e2c3ac0ea2c2e5624631c1ab9b0b0127
Miguel Urbina <miguel.urbina@michelada.io>
2019-01-22 12:15:33 -0600

    Removed the english validation on activity

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -3,6 +3,6 @@ class Activity < ApplicationRecord
   scope :user_activities, ->(actual_user) { where(user_id: actual_user) }
   enum activity_type: { Curso: 0, Platica: 1, Post: 2 }
   enum status: { "Por validar": 0, "Rechazado": 1, "Validado": 2 }
-  validates :name, :english, :location, presence: true
+  validates :name, :location, presence: true
   validates :name, uniqueness: { case_sensitive: false }
 end
