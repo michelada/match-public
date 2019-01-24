ae5c4605609b2a5b6db46ec0495be7446173a348
Miguel Urbina <miguel.urbina@michelada.io>
2019-01-24 13:31:43 -0600

    Status attribnute removed from feedback model

--- db/migrate/20190123191444_create_feedbacks.rb
+++ db/migrate/20190123191444_create_feedbacks.rb
@@ -2,7 +2,6 @@ class CreateFeedbacks < ActiveRecord::Migration[5.2]
   def change
     create_table :feedbacks do |t|
       t.string :comment
-      t.boolean :status
       t.integer :activity_id
       t.integer :user_id
 
