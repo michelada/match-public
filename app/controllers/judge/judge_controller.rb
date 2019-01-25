925809f7e705526ed5b86e1fce23e2abe02e541c
Miguel Urbina <miguel.urbina@michelada.io>
2019-01-25 16:43:24 -0600

    User is redirected whe is not judge and try to execute an judge action

--- app/controllers/judge/judge_controller.rb
+++ app/controllers/judge/judge_controller.rb
@@ -1,13 +1,11 @@
 module Judge
   class JudgeController < ActionController::Base
     before_action :authenticate_user!
-    before_action :iser_is_judge
+    before_action :user_is_judge
     layout 'judge/application'
 
-    def load_feedback_data(activity); end
-
     def user_is_judge
-      redirect_to root_path if current_user.role != 2
+      redirect_to root_path if current_user.role != 'judge'
     end
   end
 end
