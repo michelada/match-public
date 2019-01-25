5c2c5c8828745f1e52ecbe4ed130d8ea8ac4ded6
Miguel Urbina <miguel.urbina@michelada.io>
2019-01-25 16:32:26 -0600

    Judge controllers inherates from JudgeController and verify user role

--- app/controllers/judge/judge_controller.rb
+++ app/controllers/judge/judge_controller.rb
@@ -1,8 +1,13 @@
 module Judge
-  class JudgeController < ApplicationController
+  class JudgeController < ActionController::Base
     before_action :authenticate_user!
+    before_action :iser_is_judge
     layout 'judge/application'
 
     def load_feedback_data(activity); end
+
+    def user_is_judge
+      redirect_to root_path if current_user.role != 2
+    end
   end
 end
