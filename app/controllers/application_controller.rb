17c4ab284c6122682a5f2810cf32edcc9fd7fb35
Juan Brizuela <juan.brizuela@michelada.io>
2019-01-25 18:29:03 -0600

    Users can access leaderboard with no problems

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 # Main controller
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  def after_sign_in_path_for(users)
+    if current_user.team.nil?
+      new_team_path 
+    else
+      root_path
+    end
+  end
+
 end
--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,3 @@
 class MainController < ApplicationController
-  def index
-    @current_user = current_user
-    redirect_to new_team_path if @current_user.team.nil?
-  end
+  def index; end
 end
