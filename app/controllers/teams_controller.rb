c243fb79b01b333e91e9df6bdfc7199f507241ae
Miguel Urbina <miguel.urbina@michelada.io>
2019-01-27 17:32:51 -0600

    When user create a team, also can invite other users to the team

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,11 +1,13 @@
 class TeamsController < ApplicationController
+  VALID_EMAIL_REGEX = /~*@michelada.io/i.freeze
   def new
     @team = Team.new
   end
 
   def create
     @team = Team.new(team_params)
-    if @team.save && current_user.update_attribute(:team, @team)
+    if validate_user && @team.save && current_user.update_attribute(:team, @team)
+      invite_users
       flash[:notice] = t('team.messages.created')
       redirect_to root_path
     else
@@ -18,5 +20,22 @@ class TeamsController < ApplicationController
 
   def team_params
     params.require(:team).permit(:name)
+  end
+
+  def invite_users
+    user1 = params[:user_invitation_1][:email]
+    user2 = params[:user_invitation_2][:email]
+    User.invite!({ email: user1 }, current_user) unless user1.empty?
+    User.invite!({ email: user2 }, current_user) unless user2.empty?
+  end
+
+  def validate_user
+    user1 = params[:user_invitation_1][:email]
+    return false if !user1.empty? && !user1.match(VALID_EMAIL_REGEX)
+
+    user2 = params[:user_invitation_2][:email]
+    return false if !user2.empty? && !user2.match(VALID_EMAIL_REGEX)
+
+    true
   end
 end
