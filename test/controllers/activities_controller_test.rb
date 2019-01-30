84550c8b4b43147e0816f1afaf7be5381f192ab0
Leyaim Jimenez <leyaim.jimenez@michelada.io>
2019-01-30 16:48:54 -0600

    Added some test

--- test/controllers/activities_controller_test.rb
+++ test/controllers/activities_controller_test.rb
@@ -7,15 +7,40 @@ class ActivitiesControllerTest < ActionDispatch::IntegrationTest
     @user_with_team = users(:user_with_team)
   end
 
-  test 'loged user can create an activity' do
+  test 'loged user with team can create an activity' do
     sign_in @user_with_team
     post activities_path, params: { activity: { name: 'Android Studio', activity_type: 'Curso', english: 0 }, locations_string: 'UDEC,TEC' }
-    assert_redirected_to team_path(@team), 'Controller response unexpectefd'
+    assert_redirected_to team_path(@team), 'Controller response unexpected'
+  end
+
+  test 'no logged user can not create an activity' do
+    get root_path
+    assert_redirected_to new_user_session_path, 'Controller response unexpected'
   end
 
   test 'users with no team can not access create activity view' do
     sign_in @user
     get new_activity_path
     assert_redirected_to new_team_path, 'Controller response unexpected'
+  end
+
+  test 'users can not created an activity if the activity name is blank' do
+    sign_in @user_with_team
+    post activities_path, params: { activity: {name:'', activity_type: 'Curso', english: 0 }, locations_string: 'UDC' }
+  end
+
+  test 'users without teams redirect to create team.' do
+    sign_in @user
+    get new_team_path
+  end
+
+  test 'no logged user can view main page.' do
+    sign_in @user
+    get root_path
+  end
+
+  test 'users can invite another user.' do
+    sign_in @user_with_team
+    get new_user_invitation 
   end
 end
--- test/controllers/main_controller_test.rb
+++ test/controllers/main_controller_test.rb
@@ -5,12 +5,12 @@ class MainControllerTest < ActionDispatch::IntegrationTest
     @user = users(:user)
   end
 
-  test 'no logged user can not visit root path' do
+  test 'no logged user can not visit main page' do
     get root_path
     assert_redirected_to new_user_session_path, 'Controller response unexpected'
   end
 
-  test 'logged user can visit root path' do
+  test 'logged user can visit main page' do
     @user_with_team = users(:user_with_team)
     sign_in @user_with_team
     get root_path
@@ -26,6 +26,11 @@ class MainControllerTest < ActionDispatch::IntegrationTest
   test 'logged user with no team can not add a new activity' do
     sign_in @user
     get new_activity_path
+    assert_redirected_to new_team_path, 'Controller response unexpected'
+  end
+  test 'logged users can view main page' do
+    sign_in @user
+    get 
     assert_redirected_to new_team_path, 'Controller response unexpected'
   end
 end
