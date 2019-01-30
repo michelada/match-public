2579b3f13d38e3eb0276d70088d5fd8a4d2736bd
Galilea Lopez <galilealopez@MBP-de-Galilea.localdomain>
2019-01-29 18:24:49 -0600

    Only judge can see the link to judge_index on header

--- app/controllers/judge/judge_controller.rb
+++ app/controllers/judge/judge_controller.rb
@@ -2,7 +2,7 @@ module Judge
   class JudgeController < ActionController::Base
     before_action :authenticate_user!
     before_action :user_is_judge
-    layout 'judge/application'
+    layout 'application'
 
     def user_is_judge
       redirect_to main_index_path  if current_user.role != 'judge'
