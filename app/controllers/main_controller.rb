fdf80f085221c804d48c6a582edba99c7e54d3f6
Juan Brizuela <juan.brizuela@michelada.io>
2019-01-30 12:25:44 -0600

    Main view finished

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,3 +1,6 @@
 class MainController < ApplicationController
-  def index; end
+  def index
+    @top_teams = Activity.team_score(6)
+    @latest_activities = Activity.latest_activities
+  end
 end
--- app/models/activity.rb
+++ app/models/activity.rb
@@ -21,15 +21,21 @@ class Activity < ApplicationRecord
   enum activity_type: { Curso: 0, Platica: 1, Post: 2 }
   enum status: { "Por validar": 0, "En revisión": 1, "Aprobado": 2 }
   scope :user_activities, ->(actual_user) { where(user_id: actual_user) }
-  scope :checked_activities, ->(actual_user) { joins(:activity_statuses).where('activity_statuses.user_id = ?', actual_user).select('activities.id') }
+  scope :checked_activities, ->(actual_user) { joiwns(:activity_statuses).where('activity_statuses.user_id = ?', actual_user).select('activities.id') }
   scope :pending_activities, ->(actual_user) { where('activities.id NOT IN (?)', checked_activities(actual_user)) }
   scope :team_activities, ->(team_id) { joins(:user).where('users.team_id = ?', team_id) }
-  scope :team_score, (lambda {
+  scope :team_activities_score, ->(team_id) { team_activities(team_id).sum('score') }
+  # THESE VALUES WHERE CHANGED FOR TEST PORPUSES ---------------> <------ CHANGE'EM TO 2 BEFORE COMMITING
+  scope :latest_activities, -> { where('activities.status = ?', 2).order('created_at DESC limit 3') }
+  scope :total_score, -> { sum('score') }
+  scope :team_score, (lambda { |team_count|
+    # change this value to 2 ------> <-------
     where('activities.status = ?', 2)
       .joins(:user).joins('INNER JOIN teams ON users.team_id = teams.id')
-      .group('teams.name')
-      .select('teams.name as name, sum(activities.score) as total_score')
-      .order('total_score DESC LIMIT 5')
+      .group('teams.name, teams.id')
+      .select('teams.name as name, teams.id as id, sum(activities.score) as total_score')
+      .order('total_score DESC')
+      .limit(team_count)
   })
   validates :name, presence: true
   validates :name, uniqueness: { case_sensitive: false }
