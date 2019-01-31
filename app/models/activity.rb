f12aa615903d488a6d8af5e93747a703128a95e0
Miguel Urbina <miguel.urbina@michelada.io>
2019-01-30 18:45:49 -0600

    User can upload a file into the activity

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -20,6 +20,7 @@ class Activity < ApplicationRecord
   has_many :activity_statuses
   enum activity_type: { Curso: 0, Platica: 1, Post: 2 }
   enum status: { "Por validar": 0, "En revisión": 1, "Aprobado": 2 }
+  mount_uploader :activity_file, ActivityFileUploader
   scope :user_activities, ->(actual_user) { where(user_id: actual_user) }
   scope :checked_activities, ->(actual_user) { joins(:activity_statuses).where('activity_statuses.user_id = ?', actual_user).select('activities.id') }
   scope :pending_activities, ->(actual_user) { where('activities.id NOT IN (?)', checked_activities(actual_user)) }
