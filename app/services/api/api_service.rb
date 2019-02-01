fa08eef835e9957b5643b7fe0634ff17b8a1ffec
Miguel Urbina <miguel.urbina@michelada.io>
2019-02-01 14:27:14 -0600

    Rubocop fixed

--- app/services/api/api_service.rb
+++ app/services/api/api_service.rb
@@ -16,7 +16,7 @@ module Api
     def last_activity_format(activity)
       team_name = activity.user.team.name
       activity_location = get_activity_location(activity)
-      activity_type = activity.activity_type == "Post" ? "Post" : get_activity_type_en(activity.activity_type)
+      activity_type = activity.activity_type == 'Post' ? 'Post' : get_activity_type_en(activity.activity_type)
       response = obtain_label_object.clone
       response['data'] = []
       postfix = "Team #{team_name} - #{activity_type} #{activity_location}"
@@ -30,11 +30,11 @@ module Api
     private
 
     def get_activity_location(activity)
-      activity.locations[0] ? "at #{activity.locations.first.name}" : ""
+      activity.locations[0] ? "at #{activity.locations.first.name}" : ''
     end
 
     def get_activity_type_en(activity_type)
-      activity_type == "Curso" ? "Course" : "Talk"
+      activity_type == 'Curso' ? 'Course' : 'Talk'
     end
 
     def obtain_base_object
