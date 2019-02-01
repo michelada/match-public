f1221048b61b867db4a4b1b7d02accd28ae05489
Miguel Urbina <miguel.urbina@michelada.io>
2019-01-31 18:19:02 -0600

    domain parametter changed into production envinronment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
   ActionMailer::Base.smtp_settings = {
     user_name: ENV['SENDGRID_USERNAME'],
     password: ENV['SENDGRID_PASSWORD'],
-    domain: 'example.io',
+    domain: 'https://git.heroku.com/mcm-stage.git',
     address: 'smtp.sendgrid.net',
     port: 587,
     authentication: :plain,
