4bdcbb228a5c1d17535e771a16fcd1b6e6717111
Juan Brizuela <juan.brizuela@michelada.io>
2019-02-04 11:21:32 -0600

    Removed description input area from activities-form

--- app/assets/javascripts/activity.js
+++ app/assets/javascripts/activity.js
@@ -42,23 +42,11 @@ function changeLabelsValues(pitchAudienceText, abstractOutlineText) {
 }
 
 function removeLabelsAndInputs() {
-  $('#pitch-audience, #abstract-outline').hide();
+  $('#pitch-audience, #abstract-outline, #description').hide();
 }
 
 function addRemovedFields(){
   if ($('.optional-fields').html().trim() == ""){
-    $('.optional-fields').append('<div id="pitch-audience">\
-      <div class="form-group text optional activity_pitch_audience">\
-        <label class="form-control-label text optional" for="activity_pitch_audience">Pitch</label>\
-        <textarea class="form-control text optional input-text" name="activity[pitch_audience]" id="activity_pitch_audience"></textarea>\
-      </div>\
-    </div>\
-    <div id="abstract-outline">\
-      <div class="form-group text optional activity_abstract_outline">\
-        <label class="form-control-label text optional" for="activity_abstract_outline">Abstract</label>\
-        <textarea class="form-control text optional input-text" name="activity[abstract_outline]" id="activity_abstract_outline"></textarea>\
-      </div>\
-    </div>');
+    $('#pitch-audience, #abstract-outline, #description').show();
   }
-  $('#pitch-audience, #abstract-outline').show();
 }
