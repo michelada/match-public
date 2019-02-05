b326bcf9861cf780001c22d31c3bbeb34f062d53
Juan Brizuela <juan.brizuela@michelada.io>
2019-02-05 10:48:34 -0600

    Registered users can now be added to a team only if they don't have one

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -5,7 +5,9 @@ module Users
     after_action :update_user_account, only: [:create]
     def update_user_account
       @user = User.find_by_email(params[:user][:email])
-      @user&.update_attributes(team_id: current_user.team_id, role: User.roles[:user])
+      if @user.team.nil?
+        @user&.update_attributes(team_id: current_user.team_id, role: User.roles[:user])
+      end
     end
   end
 end
