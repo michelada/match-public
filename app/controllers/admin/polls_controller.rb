dbc36baa0ef9d3cda974ee7d19e6cf297cb5ef0c
Miguel Urbina <miguel.urbina@michelada.io>
2019-02-06 16:01:42 -0600

    Admin user can create, delete and update a poll

--- app/controllers/admin/polls_controller.rb
+++ app/controllers/admin/polls_controller.rb
@@ -1,11 +1,57 @@
 module Admin
   class PollsController < AdminController
     def index
-      Poll.all
+      @polls = Poll.all
     end
 
     def new
       @poll = Poll.new
+    end
+
+    def edit
+      @poll = Poll.find(params[:id])
+    end
+
+    def create
+      @poll = Poll.new(poll_params)
+      if @poll.save
+        flash[:notice] = t('poll.created')
+        redirect_to admin_polls_path
+      else
+        flash[:aleert] = t('poll.error_creating')
+        render 'new'
+      end
+    end
+
+    def show
+      @poll = Poll.find(params[:id])
+    end
+
+    def update
+      @poll = Poll.find(params[:id])
+      if @poll.update(poll_params)
+        flash[:notice] = t('polls.updated')
+        redirect_to admin_polls_path
+      else
+        flash[:alert] = t('polls.error_updating')
+        render 'edit'
+      end
+    end
+
+    def destroy
+      @poll = Poll.find(params[:id])
+      if @poll.destroy
+        flash[:notice] = t('polls.deleted')
+      else
+        flash[:alert] = t('polls.erorr_deleting')
+      end
+      redirect_to admin_polls_path
+    end
+
+    private
+
+    def poll_params
+      params.require(:poll).permit(:end_date, :start_date)
     end
   end
 end
