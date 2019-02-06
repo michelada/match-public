fdb939d47c34fd00e2ded9e92d665fc3a30d2c9f
Miguel Urbina <miguel.urbina@michelada.io>
2019-02-05 19:56:40 -0600

    Judge can approve location and english

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_04_200534) do
+ActiveRecord::Schema.define(version: 2019_02_05_221329) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,11 @@ ActiveRecord::Schema.define(version: 2019_02_04_200534) do
     t.string "activity_file"
     t.boolean "english_approve"
     t.index ["user_id"], name: "index_activities_on_user_id"
+  end
+
+  create_table "activities_locations", id: false, force: :cascade do |t|
+    t.bigint "activity_id", null: false
+    t.bigint "location_id", null: false
   end
 
   create_table "activity_statuses", force: :cascade do |t|
@@ -63,11 +68,17 @@ ActiveRecord::Schema.define(version: 2019_02_04_200534) do
     t.index ["activity_id"], name: "index_locations_on_activity_id"
   end
 
+  create_table "polls", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "teams", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "score"
   end
 
   create_table "users", force: :cascade do |t|
@@ -77,10 +88,6 @@ ActiveRecord::Schema.define(version: 2019_02_04_200534) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "invitation_token"
@@ -97,7 +104,7 @@ ActiveRecord::Schema.define(version: 2019_02_04_200534) do
     t.index ["invitation_token"], name: "index_users_on_invitation_token", unique: true
     t.index ["invitations_count"], name: "index_users_on_invitations_count"
     t.index ["invited_by_id"], name: "index_users_on_invited_by_id"
-    t.index %w[invited_by_type invited_by_id], name: "index_users_on_invited_by_type_and_invited_by_id"
+    t.index ["invited_by_type", "invited_by_id"], name: "index_users_on_invited_by_type_and_invited_by_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["team_id"], name: "index_users_on_team_id"
   end
