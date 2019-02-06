8a67cb8fa1674179e242821e1888f2ded44532a2
Juan Brizuela <juan.brizuela@michelada.io>
2019-02-05 23:47:00 -0600

    Invites are now supported from team_show invite link

--- app/controllers/team_invitations_controller.rb
+++ app/controllers/team_invitations_controller.rb
@@ -1,18 +1,48 @@
 class TeamInvitationsController < ApplicationController
-  def create
-    render 
+  VALID_EMAIL_REGEX = /~*@michelada.io/i.freeze
+
+  def new
+    render "new"
   end
+  
+  def create
+    if validate_user && invite_user
+      flash[:notice] = "Usuario agregado correctamente"
+      redirect_to team_path(current_user.team)
+    else
+      render new_team_path
+    end
+  end
+  
 
   private
 
-  def team_params
-    params.require(:team).permit(:name)
+  def team_invitations_params
+    params.permit(:email)
   end
 
-  def invite_users
-    user1 = params[:user_invitation_1][:email]
-    team_id = current_user.team_id
-    User.find_by(email: user1).update_attribute(:team_id, team_id) if User.exists?(email: user1)
-    User.invite!({ email: user1 }, current_user) unless user1.empty?
+  def invite_user
+    user_email = params[:email]
+    user = User.find_by(email: user_email)
+    unless user.nil?
+      if user.team.nil?
+        user.update_attributes(team_id: current_user.team_id)
+      else
+        flash[:alert] = "Este usuario ya tiene un equipo"
+        return false
+      end
+    else
+      User.invite!({ email: user_email }, current_user)
+    end
+  end
+
+  def validate_user
+    user = params[:email]
+    if user.empty? || !user.match(VALID_EMAIL_REGEX)
+      flash[:alert] = "El email ingresado no es valido"
+      return false 
+    end
+
+    return true
   end
 end
