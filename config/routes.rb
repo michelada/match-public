1befe82cf107a84f82d101248fc5879dff655aff
Leyaim Jimenez <leyaim.jimenez@michelada.io>
2019-02-11 19:37:07 -0600

    Added new route

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,8 @@ Rails.application.routes.draw do
   as :user do
     get '/users' => 'devise_invitable/registrations#new'
     get '/teams' => 'teams#new'
-    get '/activities' => 'activities#new'
     get '/team_invitation' => 'team_invitations#new'
+    get '/activities' => 'activities#new'
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'landing_page#index'
