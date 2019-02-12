ccc69a915f718e8622c06b16c1c29ed62657d1ca
Miguel Urbina <miguel.urbina@michelada.io>
2019-02-12 08:07:41 -0600

    Only normal users can access to /poll

--- app/controllers/polls_controller.rb
+++ app/controllers/polls_controller.rb
@@ -1,7 +1,14 @@
 class PollsController < ApplicationController
+  before_action :user_can_access
   def index
     @poll = Poll.all.first
     @activities = Activity.from_a_poll(@poll.activities_from, @poll.activities_to)
     @activity_types = @activities.group(:activity_type).select(:activity_type)
+  end
+
+  private
+
+  def user_can_access
+    redirect_to root_path unless current_user.role == 'user'
   end
 end
