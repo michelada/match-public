bc04cdead2cf5ab4387ac2cc7ed5b93bf0fdc061
Miguel Urbina <miguel.urbina@michelada.io>
2019-02-13 16:24:33 -0600

    User is redirected to new activity path when access to root path

--- app/controllers/landing_page_controller.rb
+++ app/controllers/landing_page_controller.rb
@@ -1,6 +1,6 @@
 class LandingPageController < ApplicationController
   skip_before_action :authenticate_user!
   def index
-    redirect_to main_index_path unless current_user.nil?
+    redirect_to new_activity_path unless current_user.nil?
   end
 end
