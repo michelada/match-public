f48f11d6848410f90302b9c77b197f70d1a54b4e
Miguel Urbina <miguel.urbina@michelada.io>
2019-02-15 11:10:33 -0600

    Domain chanhed in production enviroment

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -73,7 +73,7 @@ Rails.application.configure do
   ActionMailer::Base.smtp_settings = {
     user_name: ENV['SENDGRID_USERNAME'],
     password: ENV['SENDGRID_PASSWORD'],
-    domain: 'https://git.heroku.com/mcm-stage.git',
+    domain: 'mcm-stage.heroku.com',
     address: 'smtp.sendgrid.net',
     port: 587,
     authentication: :plain,
