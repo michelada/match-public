d942be64337318cf62179632029d69df1faba72b
Miguel Urbina <miguel.urbina@michelada.io>
2019-02-15 14:47:10 -0600

    Fixed user role assign when registering

--- app/controllers/invitation_controller.rb
+++ app/controllers/invitation_controller.rb
@@ -2,6 +2,6 @@ class InvitationController < DeviseInvitable::RegistrationsController
   after_action :update_user_role, only: [:create]
 
   def update_user_role
-    current_user.update_attributes(role: 0)
+    current_user&.update_attributes(role: 0)
   end
 end
