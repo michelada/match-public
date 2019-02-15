8b1db7505c896944e38442acb945d064f0d880ee
Miguel Urbina <miguel.urbina@michelada.io>
2019-02-15 11:29:49 -0600

    Mailgun now does not uses domain variable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,6 @@ Rails.application.configure do
     address: ENV['MAILGUN_SMTP_SERVER'],
     user_name: ENV['MAILGUN_SMTP_LOGIN'],
     password: ENV['MAILGUN_SMTP_PASSWORD'],
-    domain: ENV['MAILGUN-DOMAIN'],
     authentication: :plain
   }
 
