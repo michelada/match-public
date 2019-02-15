cbb3ac006fa84aa0e5537b62f55501bb913e43ea
Galilea López <galilea.lopez@michelada.io>
2019-02-15 21:44:30 +0000

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,6 @@ Rails.application.routes.draw do
     get '/team_invitation' => 'team_invitations#new'
     get '/activities' => 'activities#new'
   end
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'landing_page#index'
   namespace :judge do
