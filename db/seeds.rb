b25a45a207b4729f4fa9d5e72d45bd511e372ce0
Miguel Urbina <miguel.urbina@michelada.io>
2019-02-14 22:41:57 -0600

    Updated database seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
 unless User.exists?('admin_user@michelada.io')
-  User.create(email: 'admin_user@michelada.io', password: 'adminUSer')
-end
+    User.create(email: 'admin_user@michelada.io', password: 'adminUser',
+                name: 'Usuario Administrador', role: 2)
+  end
