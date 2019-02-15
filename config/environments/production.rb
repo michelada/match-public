23635e6d49642016ef237867d08b1ebdcf86f164
Miguel Urbina <miguel.urbina@michelada.io>
2019-02-15 11:38:53 -0600

    Last update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,12 +71,12 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   ActionMailer::Base.smtp_settings = {
-    port: ENV['MAILGUN_SMTP_PORT'],
     address: ENV['MAILGUN_SMTP_SERVER'],
-    user_name: ENV['MAILGUN_SMTP_LOGIN'],
-    password: ENV['MAILGUN_SMTP_PASSWORD'],
+    authentication: :plain,
     domain: ENV['MAILGUN_DOMAIN'],
-    authentication: :plain
+    port: ENV['MAILGUN_SMTP_PORT'],
+    password: ENV['MAILGUN_SMTP_PASSWORD'],
+    user_name: ENV['MAILGUN_SMTP_LOGIN']
   }
 
   ActionMailer::Base.delivery_method = :smtp
