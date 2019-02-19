5ae9e3f4507f6aa6907d3981bf51b7bbcb6bb851
Galilea Lopez <galilealopez@MBP-de-Galilea.localdomain>
2019-02-19 17:02:01 -0600

    notifications gem

--- Gemfile
+++ Gemfile
@@ -24,6 +24,7 @@ gem 'simple_form', '~>4.0.1'
 gem 'spicy-proton'
 gem 'turbolinks', '~> 5'
 gem 'uglifier', '>= 1.3.0'
+gem 'activity_notification'
 
 group :development, :test do
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_12_221014) do
+ActiveRecord::Schema.define(version: 2019_02_19_002822) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,11 @@ ActiveRecord::Schema.define(version: 2019_02_12_221014) do
     t.string "activity_file"
     t.boolean "english_approve"
     t.index ["user_id"], name: "index_activities_on_user_id"
+  end
+
+  create_table "activities_locations", id: false, force: :cascade do |t|
+    t.bigint "activity_id", null: false
+    t.bigint "location_id", null: false
   end
 
   create_table "activity_statuses", force: :cascade do |t|
@@ -62,6 +67,28 @@ ActiveRecord::Schema.define(version: 2019_02_12_221014) do
     t.index ["activity_id"], name: "index_locations_on_activity_id"
   end
 
+  create_table "notifications", force: :cascade do |t|
+    t.string "target_type", null: false
+    t.bigint "target_id", null: false
+    t.string "notifiable_type", null: false
+    t.bigint "notifiable_id", null: false
+    t.string "key", null: false
+    t.string "group_type"
+    t.bigint "group_id"
+    t.integer "group_owner_id"
+    t.string "notifier_type"
+    t.bigint "notifier_id"
+    t.text "parameters"
+    t.datetime "opened_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["group_owner_id"], name: "index_notifications_on_group_owner_id"
+    t.index ["group_type", "group_id"], name: "index_notifications_on_group_type_and_group_id"
+    t.index ["notifiable_type", "notifiable_id"], name: "index_notifications_on_notifiable_type_and_notifiable_id"
+    t.index ["notifier_type", "notifier_id"], name: "index_notifications_on_notifier_type_and_notifier_id"
+    t.index ["target_type", "target_id"], name: "index_notifications_on_target_type_and_target_id"
+  end
+
   create_table "polls", force: :cascade do |t|
     t.date "start_date", null: false
     t.date "end_date", null: false
@@ -69,6 +96,24 @@ ActiveRecord::Schema.define(version: 2019_02_12_221014) do
     t.datetime "updated_at", null: false
     t.date "activities_from", null: false
     t.date "activities_to", null: false
+  end
+
+  create_table "subscriptions", force: :cascade do |t|
+    t.string "target_type", null: false
+    t.bigint "target_id", null: false
+    t.string "key", null: false
+    t.boolean "subscribing", default: true, null: false
+    t.boolean "subscribing_to_email", default: true, null: false
+    t.datetime "subscribed_at"
+    t.datetime "unsubscribed_at"
+    t.datetime "subscribed_to_email_at"
+    t.datetime "unsubscribed_to_email_at"
+    t.text "optional_targets"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["key"], name: "index_subscriptions_on_key"
+    t.index ["target_type", "target_id", "key"], name: "index_subscriptions_on_target_type_and_target_id_and_key", unique: true
+    t.index ["target_type", "target_id"], name: "index_subscriptions_on_target_type_and_target_id"
   end
 
   create_table "teams", force: :cascade do |t|
@@ -99,7 +144,7 @@ ActiveRecord::Schema.define(version: 2019_02_12_221014) do
     t.index ["invitation_token"], name: "index_users_on_invitation_token", unique: true
     t.index ["invitations_count"], name: "index_users_on_invitations_count"
     t.index ["invited_by_id"], name: "index_users_on_invited_by_id"
-    t.index %w[invited_by_type invited_by_id], name: "index_users_on_invited_by_type_and_invited_by_id"
+    t.index ["invited_by_type", "invited_by_id"], name: "index_users_on_invited_by_type_and_invited_by_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["team_id"], name: "index_users_on_team_id"
   end
