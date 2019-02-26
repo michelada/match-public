03bc4bc18d39834c70dc6fc75bb6d7a16a73621c
Juan Brizuela <juan.brizuela@michelada.io>
2019-02-26 15:54:36 -0600

    Implemented 3 sections for judges activities

---
+++ app/assets/javascripts/judge/main_index.js
@@ -0,0 +1,17 @@
+$(document).on('turbolinks:load', function(){
+  $('.activitiesContainer').hide();
+  $('.activitiesContainer:first').show();
+  $('a.activePill').css('color','#FFD557');
+
+  $('a.button.pill').on('click', function(ev){
+    ev.preventDefault();
+    $('a.activePill').removeClass('inactivePill');
+    $('a.activePill').css('color','#fff');
+    $('a.activePill').removeClass('activePill');
+    $(this).addClass('activePill');
+    $(this).removeClass('inactivePill');
+    $(this).css('color','#FFD557');
+    $('div.activitiesContainer').hide();
+    $($(this).attr('href')).show();
+  })
+})
