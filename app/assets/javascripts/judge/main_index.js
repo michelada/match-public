66f191f16f1f7a4bedf4a700cf179e2637242b31
Miguel Urbina <miguel.urbina@michelada.io>
2019-02-26 16:34:12 -0600

    Remove no needed code. Highligh sections with css class

--- app/assets/javascripts/judge/main_index.js
+++ app/assets/javascripts/judge/main_index.js
@@ -1,15 +1,13 @@
 $(document).on('turbolinks:load', function(){
   $('.activitiesContainer').hide();
   $('.activitiesContainer:first').show();
-  $('a.activePill').css('color','#FFD557');
 
   $('a.button.pill').on('click', function(ev){
     ev.preventDefault();
-    $('a.activePill').css('color','#fff');
     $('a.activePill').removeClass('activePill');
     $(this).addClass('activePill');
-    $(this).css('color','#FFD557');
     $('div.activitiesContainer').hide();
     $($(this).attr('href')).show();
   })
+
 })
