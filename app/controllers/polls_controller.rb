fd8b2c6ec82714c03213cd7342f0ab571b178502
Miguel Urbina <miguel.urbina@michelada.io>
2019-02-28 16:58:08 -0600

    Judge votes fixed

--- app/controllers/polls_controller.rb
+++ app/controllers/polls_controller.rb
@@ -15,7 +15,6 @@ class PollsController < ApplicationController
     @activity_types = @activities.group(:activity_type).select(:activity_type)
     @best_activities = []
     3.times { |i| @best_activities << Activity.best_activities(@poll.id, i) }
-
     return unless @activities.empty?
 
     flash[:alert] = t('poll.empty_activities')
