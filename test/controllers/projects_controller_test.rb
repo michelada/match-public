bf3fe650d415c9c938bab9cd43ed95b34571294d
Miguel Urbina <miguel.urbina@michelada.io>
2019-04-26 15:17:02 -0500

    Added tests to verofy that user can upload/update a project

--- test/controllers/projects_controller_test.rb
+++ test/controllers/projects_controller_test.rb
@@ -5,20 +5,20 @@ class ProjectsControllerTest < ActionDispatch::IntegrationTest
     @match = matches(:active_project_match)
   end
 
-  test 'no logged user cannot create a new activity' do
+  test 'no logged user cannot access to new activity view' do
     get new_match_project_path(@match)
 
     assert_redirected_to new_user_session_path, 'Controller response unexpected'
   end
 
-  test 'logged user without project can create a new project' do
+  test 'logged user without project can access to new project view' do
     sign_in users(:user_with_team)
     get new_match_project_path(@match)
 
     assert_response :success, 'Controller response unexpected'
   end
 
-  test 'logged user with a project cannot create a new project' do
+  test 'logged user with a project cannot access to new project view' do
     user = users(:user_test1)
     sign_in user
 
@@ -28,12 +28,84 @@ class ProjectsControllerTest < ActionDispatch::IntegrationTest
     assert_equal flash[:alert], I18n.t('projects.already_have_one')
   end
 
-  test 'users with no team cannot create a project' do
+  test 'users with no team cannot access to new project view' do
     sign_in users(:user)
 
     get new_match_project_path(@match)
 
     assert_redirected_to new_match_team_path(@match)
     assert_equal flash[:alert], I18n.t('projects.no_team')
+  end
+
+  test 'logged user with team can create a new project' do
+    user = users(:user_with_team)
+    sign_in user
+
+    post match_projects_path(@match), params: { project: { name: 'Example project',
+                                                           description: 'Random description for the example project',
+                                                           repositories: 'GitHub-repo, GitLab-repo',
+                                                           features: 'User session',
+                                                           match: @match,
+                                                           team: user.team } }
+    assert_redirected_to match_team_path(@match, user.team)
+    assert_equal flash[:notice], I18n.t('projects.created')
+  end
+
+  test 'logged user with team cannot create a new project without description' do
+    user = users(:user_with_team)
+    sign_in user
+
+    post match_projects_path(@match), params: { project: { name: 'Example project',
+                                                           repositories: 'GitHub-repo, GitLab-repo',
+                                                           features: 'User session',
+                                                           match: @match,
+                                                           team: user.team } }
+    assert_response :success
+    assert_equal flash[:alert], I18n.t('projects.error_creating')
+  end
+
+  test 'logged user with team cannot create a new project without name' do
+    user = users(:user_with_team)
+    sign_in user
+
+    post match_projects_path(@match), params: { project: { description: 'Random description for the example project',
+                                                           repositories: 'GitHub-repo, GitLab-repo',
+                                                           features: 'User session',
+                                                           match: @match,
+                                                           team: user.team } }
+    assert_response :success
+    assert_equal flash[:alert], I18n.t('projects.error_creating')
+  end
+
+  test 'user can update project attributes' do
+    user = users(:user_test1)
+    sign_in user
+
+    put match_project_path(@match, user.team.project), params: { project: { name: 'A new project name',
+                                                                            description: 'Random description',
+                                                                            repositories: 'GitHub-repo, GitLab-repo',
+                                                                            features: 'User session',
+                                                                            match: @match,
+                                                                            team: user.team } }
+
+    assert_redirected_to match_team_path(@match, user.team)
+    assert_equal flash[:notice], I18n.t('projects.updated')
+  end
+
+  test 'user can not modify another one teams project' do
+    project = projects(:simple_project)
+    project.update_attributes(match_id: 6)
+
+    user = users(:user_test1)
+    sign_in user
+
+    put match_project_path(@match, project), params: { project: { name: 'A new project name',
+                                                                  description: 'Random description',
+                                                                  repositories: 'GitHub-repo, GitLab-repo',
+                                                                  features: 'User session',
+                                                                  match: @match,
+                                                                  team: user.team } }
+    assert_equal flash[:alert], I18n.t('activities.messages.no_permitted')
+    assert_redirected_to new_match_team_path(@match)
   end
 end
