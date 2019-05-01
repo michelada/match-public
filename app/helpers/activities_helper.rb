aee1ae75f8ae6d00217ebb6c43838fca8198bcb8
Juan Brizuela <juan.brizuela@michelada.io>
2019-05-01 16:11:39 -0500

    Fixed small issues with projects form

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -1,10 +1,4 @@
 module ActivitiesHelper
-  def user_can_comment?(activity)
-    return true if current_user.judge?
-
-    activity.user.team_id == current_user.team_id
-  end
-
   def judges_appproves(activity)
     activity.activity_statuses.collect { |status| status.user.email.remove('@michelada.io') }.join(', ')
   end
