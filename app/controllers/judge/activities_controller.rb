f3a1377a08b1d1bb3696ea0e9d2599f0fc5c49f6
Leyaim Jimenez <leyaim.jimenez@michelada.io>
2019-05-13 11:49:19 -0500

    fixed typo and remove dots

--- app/controllers/judge/activities_controller.rb
+++ app/controllers/judge/activities_controller.rb
@@ -13,7 +13,7 @@ module Judge
     def update
       @activity = Activity.friendly.find(params[:id])
       @activity.update_attributes(english_approve: !@activity.english_approve)
-      message = @activity.english_approve ? t('labels.english_aproved') : t('labels.english_unaproved')
+      message = @activity.english_approve ? t('labels.english_approved') : t('labels.english_unapproved')
       flash[:notice] = message
       redirect_to match_judge_activity_path(@match, @activity)
     end
--- test/system/judge/judge_activity_test.rb
+++ test/system/judge/judge_activity_test.rb
@@ -57,14 +57,14 @@ class JudgeActivityTest < ApplicationSystemTestCase
     activity = activities(:activity_talk)
     visit match_judge_activity_path(@match, activity)
     find("a[href='/match/#{@match.id}/judge/activities/#{activity.slug}/locations/#{activity.locations.first.id}']").click
-    assert page.has_content?(I18n.t('labels.location_aproved'))
+    assert page.has_content?(I18n.t('labels.location_approved'))
   end
 
   test 'judge can unapprove an activity location' do
     activity = activities(:activity_workshop)
     visit match_judge_activity_path(@match, activity)
     find("a[href='/match/#{@match.id}/judge/activities/#{activity.slug}/locations/#{activity.locations.first.id}']").click
-    assert page.has_content?(I18n.t('labels.location_unaproved'))
+    assert page.has_content?(I18n.t('labels.location_unapproved'))
   end
 
   test 'judge can leave a comment on the activity' do
